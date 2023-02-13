10f921a2418158d96dc2ede48da1e38195dda4b0
tilitihub <tibor.tiliczki@gmail.com>
2023-02-13 22:52:49 +0100

    módosítás hozzáadva

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 /*
-* File: style.css
+* File: app.js
 * Author: Tiliczki Tibor
 * Copyright: kopirájtkriptobájt
 * Group: Szoft I/1/E
 * Date: 2023-02-13
-* Github: https://github.com/tilitihub/
+* Github: https://github.com/tilitihub/idoMunka
 * Licenc: GNU GPL
 */
+const lengthElem = document.querySelector('#length');
+const feedElem = document.querySelector('#feed');
+const rpmElem = document.querySelector('#rpm');
+const calcButton = document.querySelector('#calcButton');
+const areaElem = document.querySelector('#area');
+
+calcButton.addEventListener('click', () => {
+    
+    let length = Number(lengthElem.value);
+    let feed = Number(feedElem.value);
+    let rpm = Number(rpmElem.value);
+    let area = calcArea(length, feed, rpm);
+    console.log(area);
+    areaElem.value = area;
+
+});
+
+function calcArea(length, feed, rpm) {
+    let area = length/feed*rpm;
+    return area;
+}
