7970d025eb42cbaff43a06489d609c8fc8c031cb
tilitihub <tibor.tiliczki@gmail.com>
2023-02-13 20:02:44 +0100

    src mappa hozzáadva

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: style.css
+* Author: Tiliczki Tibor
+* Copyright: kopirájtkriptobájt
+* Group: Szoft I/1/E
+* Date: 2023-02-13
+* Github: https://github.com/tilitihub/
+* Licenc: GNU GPL
+*/
